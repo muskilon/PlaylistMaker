72fe4af22d1e1f1fabd2452d484b6282f7b112a5
Practicum <practicum@ya.ru>
2023-10-18 18:26:01 +0300

    Спринт 9/25 → Тема 3/5: EditText и TextWatcher → Урок 3/3 ДЗ

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,19 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
 
 class SearchActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -14,5 +22,28 @@ class SearchActivity : AppCompatActivity() {
             val back = Intent(this, MainActivity::class.java)
             startActivity(back)
         }
+
+        val textInput = findViewById<TextInputLayout>(R.id.search_bar_input)
+        val textInputEdit = findViewById<TextInputEditText>(R.id.search_bar_edit)
+        textInput.setEndIconOnClickListener {
+            textInputEdit.text?.clear()
+                textInputEdit.clearFocus()
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(textInputEdit.windowToken, 0)
+        }
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //empty
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        textInputEdit.addTextChangedListener(simpleTextWatcher)
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,21 +1,63 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
         val backArrow = findViewById<ImageView>(R.id.arrow_back)
         backArrow.setOnClickListener {
             val back = Intent(this, MainActivity::class.java)
             startActivity(back)
         }
+
+        val termsOfUseArrow = findViewById<ImageView>(R.id.terms_of_use_arrow)
+        termsOfUseArrow.setOnClickListener {
+            val termsOfUse = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.terms_of_use_url)))
+            startActivity(termsOfUse)
+        }
+
+        val shareAppIcon = findViewById<ImageView>(R.id.share_app_icon)
+        shareAppIcon.setOnClickListener {
+            val sendApp: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.share_app_url))
+            type = "text/plain"
+        }
+            val shareApp = Intent.createChooser(sendApp, null)
+            startActivity(shareApp)
+        }
+
+        val feedbackIcon = findViewById<ImageView>(R.id.write_to_support_icon)
+        feedbackIcon.setOnClickListener {
+            val sendFeedback = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse(getString(R.string.email_adress))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.email_text))
+            }
+            startActivity(sendFeedback)
+        }
+        //this block still not working correctly
+        val themeSwitcher = findViewById<SwitchCompat>(R.id.themeSwitch)
+        themeSwitcher.isChecked = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+
+        themeSwitcher.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+        }
+
     }
 }
