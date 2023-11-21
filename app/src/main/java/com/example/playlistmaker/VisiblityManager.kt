c05ca0b0ac4140a34faceb3b8f88e39689e34b6c
Practicum <practicum@ya.ru>
2023-11-21 22:44:18 +0300

    сделал you_searched.xml NestedScrollView

--- app/src/main/java/com/example/playlistmaker/VisiblityManager.kt
+++ app/src/main/java/com/example/playlistmaker/VisiblityManager.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.LinearLayout
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
 
@@ -13,7 +12,7 @@ fun SearchActivity.showSearch() {
     val notFoundPlaceholder = findViewById<FrameLayout>(R.id.not_found_placeholder)
     val noConnectionPlaceholder = findViewById<FrameLayout>(R.id.no_connection_placeholder)
     val searchResultsRecyclerView = findViewById<RecyclerView>(R.id.searchResultsRecyclerView)
-    val songHistoryView = findViewById<ConstraintLayout>(R.id.searchHistory)
+    val songHistoryView = findViewById<LinearLayout>(R.id.searchHistory)
 
     notFoundPlaceholder.visibility = View.GONE
     noConnectionPlaceholder.visibility = View.GONE
@@ -27,7 +26,7 @@ fun SearchActivity.showSongHistory() {
     val notFoundPlaceholder = findViewById<FrameLayout>(R.id.not_found_placeholder)
     val noConnectionPlaceholder = findViewById<FrameLayout>(R.id.no_connection_placeholder)
     val searchResultsRecyclerView = findViewById<RecyclerView>(R.id.searchResultsRecyclerView)
-    val songHistoryView = findViewById<ConstraintLayout>(R.id.searchHistory)
+    val songHistoryView = findViewById<LinearLayout>(R.id.searchHistory)
     notFoundPlaceholder.visibility = View.GONE
     noConnectionPlaceholder.visibility = View.GONE
     searchResultsRecyclerView.visibility = View.GONE
@@ -39,7 +38,7 @@ fun SearchActivity.showNotFound() {
     val notFoundPlaceholder = findViewById<FrameLayout>(R.id.not_found_placeholder)
     val noConnectionPlaceholder = findViewById<FrameLayout>(R.id.no_connection_placeholder)
     val searchResultsRecyclerView = findViewById<RecyclerView>(R.id.searchResultsRecyclerView)
-    val songHistoryView = findViewById<ConstraintLayout>(R.id.searchHistory)
+    val songHistoryView = findViewById<LinearLayout>(R.id.searchHistory)
     notFoundPlaceholder.visibility = View.VISIBLE
     noConnectionPlaceholder.visibility = View.GONE
     searchResultsRecyclerView.visibility = View.GONE
@@ -49,7 +48,7 @@ fun SearchActivity.showNoConnection() {
     val notFoundPlaceholder = findViewById<FrameLayout>(R.id.not_found_placeholder)
     val noConnectionPlaceholder = findViewById<FrameLayout>(R.id.no_connection_placeholder)
     val searchResultsRecyclerView = findViewById<RecyclerView>(R.id.searchResultsRecyclerView)
-    val songHistoryView = findViewById<ConstraintLayout>(R.id.searchHistory)
+    val songHistoryView = findViewById<LinearLayout>(R.id.searchHistory)
     notFoundPlaceholder.visibility = View.GONE
     noConnectionPlaceholder.visibility = View.VISIBLE
     searchResultsRecyclerView.visibility = View.GONE
