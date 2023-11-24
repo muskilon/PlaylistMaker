c53cb0575cc7ad86e54a9ba18337bc483d683020
MuskIlon <practicum@ya.ru>
2023-11-24 19:47:17 +0300

    Добавил новые поля в Track

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.Locale
 
 data class Track(
@@ -8,8 +9,15 @@ data class Track(
     val trackName: String, // Название композиции
     val artistName: String, // Имя исполнителя
     val trackTimeMillis: Long, // Продолжительность трека
-    val artworkUrl100: String // Ссылка на изображение обложки
+    val artworkUrl100: String, // Ссылка на изображение обложки
+    val collectionName: String,
+    val country: String,
+    val primaryGenreName: String,
+    val releaseDate: Date
 ){
-    val trackTime: String
-        get(){return SimpleDateFormat("mm:ss", Locale.getDefault()).format(this.trackTimeMillis) }
+    val trackTime: String get(){
+        return SimpleDateFormat("mm:ss", Locale.getDefault()).format(this.trackTimeMillis) }
+    val year: String get() {
+        return SimpleDateFormat("yyyy", Locale.getDefault()).format(this.releaseDate)
+    }
 }
