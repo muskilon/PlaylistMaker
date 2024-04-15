b3f51b2663c1c4c3158c37dc19bdf85d76d86d84
MuskIlon <practicum@ya.ru>
2024-04-15 18:37:56 +0300

    промежуточный

--- app/src/main/java/com/example/playlistmaker/di/ViewModelModules.kt
+++ app/src/main/java/com/example/playlistmaker/di/ViewModelModules.kt
@@ -4,6 +4,7 @@ import com.example.playlistmaker.medialibrary.ui.FavoritesViewModel
 import com.example.playlistmaker.medialibrary.ui.NewPlayListViewModel
 import com.example.playlistmaker.medialibrary.ui.PlaylistsViewModel
 import com.example.playlistmaker.player.ui.PlayerViewModel
+import com.example.playlistmaker.playlist.ui.SinglePlayListViewModel
 import com.example.playlistmaker.search.ui.SearchViewModel
 import com.example.playlistmaker.settings.ui.SettingsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -27,4 +28,5 @@ val viewModelModules = module {
 
     viewModel { PlaylistsViewModel(get(), get()) }
     viewModel { NewPlayListViewModel(get(), get()) }
+    viewModel { SinglePlayListViewModel(get(), get()) }
 }
--- app/src/main/java/com/example/playlistmaker/playlist/ui/SinglePlayListViewModel.kt
+++ app/src/main/java/com/example/playlistmaker/playlist/ui/SinglePlayListViewModel.kt
@@ -1,37 +1,10 @@
 package com.example.playlistmaker.playlist.ui
 
-import android.net.Uri
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.playlistmaker.medialibrary.domain.FilesInteractor
-import com.example.playlistmaker.medialibrary.domain.PlayList
 import com.example.playlistmaker.medialibrary.domain.PlayListInteractor
-import com.example.playlistmaker.medialibrary.domain.TrackList
-import kotlinx.coroutines.launch
 
 class SinglePlayListViewModel(
     private val filesInteractor: FilesInteractor,
     private val playListInteractor: PlayListInteractor
-) : ViewModel() {
-
-    fun createPlayList(title: String, description: String, uri: Uri?) {
-        viewModelScope.launch {
-            var newUri = uri
-            if (uri != null) {
-                newUri = filesInteractor.saveFile(uri)
-            }
-            playListInteractor.addPlayList(
-                PlayList(
-                    title = title,
-                    description = description,
-                    cover = newUri,
-                    id = 0L,
-                    trackCount = 0,
-                    tracks = TrackList(mutableListOf())
-                )
-            )
-            Log.d("TAG", newUri.toString())
-        }
-    }
-}
+) : ViewModel()
